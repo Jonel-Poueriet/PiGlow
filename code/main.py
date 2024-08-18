6dbae5817f13deac5d6b5b82cd856b95a89fcecc
jonelps <jpoueri1@binghamton.edu>
2024-08-17 20:08:57 -0400

    updated main to generate virtual led window, program stops when window is closed, added update_color and rgb_to_hex to grab average color and update virtual led color

--- code/main.py
+++ code/main.py
@@ -1,41 +1,63 @@
+import tkinter as tk
 from PIL import ImageGrab
-from PIL import Image
+import time
 
-#Grab current screen
-#screen = ImageGrab.grab()
-file = "4quart.png"
-screen = Image.open(file)
+#updated virtual led color
+def update_color(color):
+    canvas.delete("all")
+    canvas.create_oval(50, 50, 250, 250, fill=color, outline=color)
 
-width, height = screen.size
-print("width and height are ",width, height)
+#turns rgb color format (x, y, z) -> hex color equivalent
+def rgb_to_hex(rgb):
+    return f"#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}"
 
-#crops image into 4 quarters, ultimately this zoning should be relative to number of rgb strips program is using
+#set up led window
+root = tk.Tk()
+root.title("Virtual RGB LED")
 
-#top left
-zone_color_top_left = screen.crop((0, 0, width/2, height/2)).getcolors(maxcolors=1000000)
+canvas = tk.Canvas(root, width=300, height=300)
+canvas.pack()
 
-#top right
-zone_color_top_right = screen.crop(((width/2), 0, width, height/2)).getcolors(maxcolors=1000000)
+#runs until virtual led window is closed
+while True:
+    #Grab current screen
+    screen = ImageGrab.grab()
 
-#bottom left
-zone_color_bottom_left = screen.crop((0, (height/2), width/2, height)).getcolors(maxcolors=1000000)
+    #file = "4quart.png"
+    #screen = Image.open(file)
 
-#bottom right
-zone_color_bottom_right = screen.crop(((width/2), (height/2), width, height)).getcolors(maxcolors=1000000)
+    width, height = screen.size
+    #print("width and height are ",width, height)
 
-average_color_tl = max(zone_color_top_left, key=lambda x: x[0])[1]
-print("average color is ", average_color_tl)
+    #crops image into 4 quarters, ultimately this zoning should be relative to number of rgb strips program is using
 
-average_color_tr = max(zone_color_top_right, key=lambda x: x[0])[1]
-print("average color is ", average_color_tr)
+    #top left
+    #zone_color_top_left = screen.crop((0, 0, width/2, height/2)).getcolors(maxcolors=1000000)
+    #average_color_tl = max(zone_color_top_left, key=lambda x: x[0])[1]
+    #colorhex_tl = rgb_to_hex(average_color_tl)
 
-average_color_bl = max(zone_color_bottom_left, key=lambda x: x[0])[1]
-print("average color is ", average_color_bl)
+    #top right
+    #zone_color_top_right = screen.crop(((width/2), 0, width, height/2)).getcolors(maxcolors=1000000)
+    #average_color_tr = max(zone_color_top_right, key=lambda x: x[0])[1]
+    #colorhex_tr = rgb_to_hex(average_color_tr)
 
-average_color_br = max(zone_color_bottom_right, key=lambda x: x[0])[1]
-print("average color is ", average_color_br)
+    #bottom left
+    #zone_color_bottom_left = screen.crop((0, (height/2), width/2, height)).getcolors(maxcolors=1000000)
+    #average_color_bl = max(zone_color_bottom_left, key=lambda x: x[0])[1]
+    #colorhex_bl = rgb_to_hex(average_color_bl)
 
-Image.new("RGB", (width, height), average_color_tl).show()
-Image.new("RGB", (width, height), average_color_tr).show()
-Image.new("RGB", (width, height), average_color_bl).show()
-Image.new("RGB", (width, height), average_color_br).show()
+    #bottom right
+    #zone_color_bottom_right = screen.crop(((width/2), (height/2), width, height)).getcolors(maxcolors=1000000)
+    #average_color_br = max(zone_color_bottom_right, key=lambda x: x[0])[1]
+    #colorhex_br = rgb_to_hex(average_color_br)
+    #print("average color is ",average_color_br)
+    
+    screencolors = screen.getcolors(maxcolors=1000000)
+    average_color_total = max(screencolors, key=lambda x: x[0])[1]
+    colorhex = rgb_to_hex(average_color_total)
+    #print("hex color is ",colorhex)
+
+    update_color(colorhex)
+    root.update()
+    #root.after()  # Wait for .5 second
+    time.sleep(1)
