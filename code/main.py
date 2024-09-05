3ec3646e68e702ed2702a8e9b0df455f10b6ecf6
jonelpoueriet2@gmail.com <jonelpoueriet2@gmail.com>
2024-09-05 00:18:42 -0400

    added gradual shifting between colors for smoother look with linear interperlation

--- code/main.py
+++ code/main.py
@@ -4,6 +4,7 @@ import time
 
 #updated virtual led color
 def update_color(color):
+    #reformat virtual led window with new color
     canvas.delete("all")
     canvas.create_oval(50, 50, 250, 250, fill=color, outline=color)
 
@@ -11,21 +12,38 @@ def update_color(color):
 def rgb_to_hex(rgb):
     return f"#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}"
 
+#makes the change between colors more gradual by switching to color between old and new color
+#intermediate color depends on range, closer to 0 means closer match to old color, closer to 1 means closer match to new color
+#returns a rgb triple
+def gradual_color_change(curr_color, target_color, range):
+    return (
+        int(curr_color[0] + (target_color[0] - curr_color[0]) * range),
+        int(curr_color[1] + (target_color[1] - curr_color[1]) * range),
+        int(curr_color[2] + (target_color[2] - curr_color[2]) * range)
+    )
+
 #set up led window
 root = tk.Tk()
 root.title("Virtual RGB LED")
-
 canvas = tk.Canvas(root, width=300, height=300)
 canvas.pack()
+
+#initialize current startup color to black, this variable keeps track of old color to help fade to new color
+current_color = (0, 0, 0)
+
+#determines how "balanced" the fade from colors is, between 0-1, closer to 0 means fade color is closer to current color, closer to 1 means fade color is closer to new color
+#.5 means the leds will fade with a color between old and new
+transition_speed = .5
 
 #runs until virtual led window is closed
 while True:
     #Grab current screen
     screen = ImageGrab.grab()
-
+    
     #file = "4quart.png"
     #screen = Image.open(file)
 
+    #get screen size for screen partitions if applicable
     width, height = screen.size
     #print("width and height are ",width, height)
 
@@ -52,12 +70,22 @@ while True:
     #colorhex_br = rgb_to_hex(average_color_br)
     #print("average color is ",average_color_br)
     
+    #grab colors from current screen
     screencolors = screen.getcolors(maxcolors=1000000)
+    
+    #find average color to update
     average_color_total = max(screencolors, key=lambda x: x[0])[1]
-    colorhex = rgb_to_hex(average_color_total)
+    
+    #get intermediate between current color and new color for smooth transition
+    mid_color = gradual_color_change(current_color, average_color_total, transition_speed)
+    current_color = average_color_total
+    
+    #convert led color to hex in order to update
+    colorhex = rgb_to_hex(mid_color)
     #print("hex color is ",colorhex)
 
+    #update color
     update_color(colorhex)
     root.update()
     #root.after()  # Wait for .5 second
-    time.sleep(1)
+    #time.sleep(1)
